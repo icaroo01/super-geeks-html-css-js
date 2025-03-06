1035032c72304720d918cc14998b7832cc4a5a5f
icaro <152525227+icaroo01@users.noreply.github.com>
2025-03-06 20:04:29 -0300

    Add files via upload

--- javascript icaro/aula2.js
+++ javascript icaro/aula2.js
@@ -10,5 +10,46 @@ function somar(){
    n1 = parseInt(document.getElementById('n1').value)
    n2 = parseInt(document.getElementById('n2').value)
    soma = n1+n2
-   alert ('a soma deu '+soma)
+   //alert ('a soma deu '+soma)//
+   document.getElementById('resultado').innerHTML = 'a soma deu '+(n1+n2) 
 }
+
+function avaliar(){
+
+   nota = parseFloat(document.getElementById('nota').value)
+
+   if(nota>=0 && nota<=10){
+
+    if(nota>=6){
+        document.getElementById('resultado2').innerHTML = 'voce foi aprovado'
+       }else if(nota>=4){
+    
+        document.getElementById('resultado2').innerHTML = 'voce está de recuperação'
+      
+       }else{
+    
+        document.getElementById('resultado2').innerHTML = 'voce foi reprovado'
+    
+       }
+       
+    
+    }else{
+
+      document.getElementById('resultado2').innerHTML = 'valor invalido! , o valor deve ser entre 0 e 10'
+
+    }
+
+   }
+
+   function mensagem(){
+
+    for(i=0; i<=5; i++){
+        
+     document.getElementById('mensagem').innerHTML = i
+     
+
+    }
+
+   }
+
+   
