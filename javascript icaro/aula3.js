8de0b3b9beb038b334dedf6d5b21c2a77b25b76d
icaro <152525227+icaroo01@users.noreply.github.com>
2025-03-20 20:02:40 -0300

    Add files via upload

---
+++ javascript icaro/aula3.js
@@ -0,0 +1,28 @@
+function formatar(){
+
+    correção = document.getElementById('correcao').value
+     
+    document.getElementById('paragrafo').innerHTML = ' o texto "Boa noite" tem 9 caracteres e 2 palavras'
+
+}
+
+
+
+
+
+/*texto = "boa noite"
+
+alert(texto.lenght)
+*/
+/*
+
+.lenght    tamanho da string
+.charAt(5) caractere no indice "5"
+.indexOF('a') indice do caractere
+.slice(3,8) intervalo de caracteres
+.toUpperCase() caixa alta
+.toLowerCase() caixa baixa
+.trim() tira os espaços do inicio e do final da frase, mas nao os do meio da frase
+.split("") fatia a string com base no caractere dentro do ""
+
+*/
