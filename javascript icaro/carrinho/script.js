72b278fa0dee897ae5a6a1e2f79556f7356606e5
icaro <152525227+icaroo01@users.noreply.github.com>
2025-06-26 19:59:43 -0300

    Add files via upload

--- javascript icaro/carrinho/script.js
+++ javascript icaro/carrinho/script.js
@@ -2,9 +2,18 @@ lista = []
 
 function additem(){
 
-    item = document.getElementById('item').value
+    item = document.getElementById('item').value.trim()
+    item = item[0].toUpperCase() + item.slice(1).toLowerCase()
     document.getElementById('item').value = ''
-    lista.push(item)
+
+    if(  item == '' ){
+        alert('o campo está vazio')
+    } else if( lista.indexOf(item) != -1 ){
+        alert('Este item já foi adicionado.')
+    }else{
+        lista.push(item)
+    }
+
     carregarlista()
 }
 
@@ -18,7 +27,17 @@ function carregarlista(){
     document.getElementById('listadecompras').innerHTML = ''
     for( i = 0; i< lista.length; i++ ){
         document.getElementById('listadecompras').innerHTML += 
-        '<div>'+lista[i]+' <button onclick="removeritem('+i+')">X</button></div>'
+        '<div>'+lista[i]+' <button onclick="removeritem('+i+')">X</button> <button onclick="desceritem('+i+')" >▼</button></div>'
     }
+    document.getElementById('quantidade').innerHTML = lista.length
 
+}
+
+function limparlista(){
+    lista = []
+    carregarlista()
+}
+
+function desceritem(i){
+    
 }
